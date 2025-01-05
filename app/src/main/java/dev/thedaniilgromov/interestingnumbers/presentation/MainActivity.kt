531023dc80c87df1a97ca3c640c8bbbf06c1cca6
theDaniilGromov <thedaniilgromov@gmail.com>
2025-01-05 16:30:23 +0700

    modev activity

--- app/src/main/java/dev/thedaniilgromov/interestingnumbers/presentation/MainActivity.kt
+++ app/src/main/java/dev/thedaniilgromov/interestingnumbers/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package dev.thedaniilgromov.interestingnumbers
+package dev.thedaniilgromov.interestingnumbers.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import dev.thedaniilgromov.interestingnumbers.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
